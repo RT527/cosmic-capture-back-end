4d9b0f76d82ca6274bf6199c4aca918e2a98d53f
RT527 <rafi.talukder7@gmail.com>
2023-06-06 11:17:34 -0400

    update routes

---
+++ routes/opinions.js
@@ -0,0 +1,14 @@
+const router = require('express').Router()
+const opinionsCtrl = require('../controllers/opinions.js')
+const middleware = require('../middleware/auth.js')
+
+const { decodeUserFromToken, checkAuth } = middleware
+
+/*---------- Public Routes ----------*/
+
+
+/*---------- Protected Routes ----------*/
+router.use(decodeUserFromToken)
+
+
+module.exports = router
