6bf189789453378087dad520afe57589629cf6ef
RT527 <rafi.talukder7@gmail.com>
2023-06-11 19:58:16 -0400

    fix and update image data

--- routes/epics.js
+++ routes/epics.js
@@ -1,20 +1,28 @@
 const express = require('express')
 const axios = require('axios')
 require('dotenv').config()
-// const epicsCtrl = require('../controllers/epics')
 
 const router = express.Router()
 
 /*---------- Public Routes ----------*/
-// router.post('/', epicsCtrl.createEpic)
 
 /*---------- Protected Routes ----------*/
 
 router.get('/', async (req, res) => {
   try {
-    const imageUrl = `https://api.nasa.gov/EPIC/archive/natural/2019/05/30/png/epic_1b_20190530011359.png?api_key=${process.env.NASA_API_KEY}`
-    const caption = "This image was taken by NASA's EPIC camera onboard the NOAA DSCOVR spacecraft" 
-    const date = "2023-06-09" 
+    const response = await axios.get('https://api.nasa.gov/EPIC/archive/natural/2019/05/30/png/epic_1b_20190530011359.png', {
+      params: {
+        api_key: process.env.NASA_API_KEY,
+      },
+      responseType: 'arraybuffer',
+    })
+
+    const imageData = Buffer.from(response.data, 'binary').toString('base64')
+    const imageUrl = `data:${response.headers['content-type']};base64,${imageData}`
+
+    const caption = "This image was taken by NASA's EPIC camera onboard the NOAA DSCOVR spacecraft"
+    const date = "2023-06-09"
+
     res.json({ url: imageUrl, caption: caption, date: date })
   } catch (error) {
     console.log('Error fetching image data:', error)
@@ -23,6 +31,3 @@ router.get('/', async (req, res) => {
 })
 
 module.exports = router
-
-
-
